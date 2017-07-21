fcbf11bca4a24128f823d7a5291afba990d00fb2
Esti Shay <esti.r.shay@gmail.com>
2017-07-21 16:43:20 -0700

    commit current working version before assignment deadline

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,10 @@ function Pizza (panSize, toppings) {
   this.price = '';
 }
 
+function Orders () {
+  this.orderList = [];
+}
+
 //Create price of pizza with size and number of toppings
 Pizza.prototype.setPrice = function(panSize) {
   var totalPrice = 0
@@ -28,8 +32,6 @@ Pizza.prototype.setPrice = function(panSize) {
 //UI logic
 $(document).ready(function() {
 
-  // var orderNum = 0
-
   $(".pizza-form").submit(function(event) {
     var sizeInput = $("input:radio[name=pizza-size]:checked").val();
     var toppingsInput = []; $("input:checkbox[name=pizza-toppings]:checked").each(function(){
@@ -42,6 +44,18 @@ $(document).ready(function() {
     //Add price to constructor object using price calculator prototype
     pizzaOrder.price = pizzaOrder.setPrice();
 
+    // var fullOrder = new Orders();
+    // fullOrder.push(pizzaOrder);
+
+    // fullOrder.forEach(function() {
+    //   $(".pan-size").text(pizzaOrder.panSize);
+    //   if (toppingsInput.length === 0) {
+    //     $(".list-toppings").text("None");
+    //   } else {
+    //     $(".list-toppings").text(pizzaOrder.toppings);
+    //   }
+    //   $(".subtotals").text("$" + pizzaOrder.price);
+    // });
     $(".pan-size").text(pizzaOrder.panSize);
     if (toppingsInput.length === 0) {
       $(".list-toppings").text("None");
@@ -49,23 +63,8 @@ $(document).ready(function() {
       $(".list-toppings").text(pizzaOrder.toppings);
     }
     $(".price-total").text("$" + pizzaOrder.price);
+    
     $(".order-for-purchase").show();
-
-    // orderNum += 1
-    // var vars = {};
-    //
-    // var sizeInput = $("input:radio[name=pizza-size]:checked").val();
-    // var toppingsInput = []; $("input:checkbox[name=pizza-toppings]:checked").each(function(){
-    //   var aTopping = $(this).val();
-    //   toppingsInput.push(aTopping);
-    // });
-    //
-    // vars['newOrder' + orderNum] = "hello"
-    //
-    // // var "newOrder" + orderNum = new Pizza(sizeInput, toppingsInput);
-    // // alert(newOrder1.panSize);
-    // // alert(newOrder1.panSize);
-    // // alert(newOrder1.toppings);
     event.preventDefault();
   });
 
